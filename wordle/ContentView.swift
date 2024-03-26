2fdc52627aa2e0c1e20dbde0ae45845a9d2af670
su <su@sususu.su>
2024-03-26 18:37:16 +0800

    Initial Commit

---
+++ wordle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wordle
+//
+//  Created by 郭粟閣 on 2024/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wordle/wordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordleApp.swift
+//  wordle
+//
+//  Created by 郭粟閣 on 2024/3/26.
+//
+
+import SwiftUI
+
+@main
+struct wordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
